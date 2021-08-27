97bae092bb0d2e611b2c7d3a7089439f3ff91518
eduardom <edo.martinez01@gmail.com>
2021-08-27 14:48:52 +0200

    Add styling for Activity pages (2)

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -39,3 +39,26 @@ export const getUserData = async (userID, accessToken) => {
         console.log(error);
     }
 };
+
+export const toTimeString = (seconds) => {
+    if (seconds) {
+        return (new Date(seconds * 1000)).toUTCString().match(/(\d\d:\d\d:\d\d)/)[0];
+    } else {
+        return seconds;
+    }
+}
+
+export const metresToKm = (metres) => {
+    var km = metres / 1000;
+    return km.toFixed(1);
+}
+
+
+export const convertToKMH = (metresPerSec) => {
+    var kmPerHour = metresPerSec * 60 * 60 / 1000;
+    return kmPerHour.toFixed(2);
+}
+
+export const numsInRange = (start, end) => {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
